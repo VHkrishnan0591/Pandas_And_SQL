741f91d7a894b3df92e67c74be5d9f0a557e8b0c
VHkrishnan0591 <harikrishnanv0591@gmail.com>
2024-03-28 03:30:34 +0530

    Commiting the pandas code for Invalid Tweets Problem

---
+++ Invalid-Tweets.py
@@ -0,0 +1,5 @@
+import pandas as pd
+data = [[1, 'Vote for Biden'], [2, 'Let us make America great again!']]
+tweets = pd.DataFrame(data, columns=['tweet_id', 'content']).astype({'tweet_id':'Int64', 'content':'object'})
+df = tweets[(tweets['content'].str.len())>15][['tweet_id']]
+print(df)
